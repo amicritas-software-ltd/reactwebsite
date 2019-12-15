69e68cdf46f7ceb86ee5df2b467f2bef9db0b67a
Md Sabbir Ahmed <md.sabbir073@gmail.com>
2019-12-15 19:45:44 +0600

    updated file

--- src/pages/Presentation.js
+++ src/pages/Presentation.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import Header from "../components/header/Header";
 import Pagetop from "../components/pages/Pagetop";
 import Footer from "../components/footer/Footer";
 import Bottomfooter from "../components/footer/Bottomfooter";
-import PresentationInfo from "../components/pages/presentation/PresentationInfo";
+import Presentationcomponent from "../components/pages/presentation/Presentationcomponent";
 
 class Presentation extends Component {
-    state = {
-        title: "Jobs",
-        background:
-          "http://new.uouapps.com/iod-html/html/images/bg/sub-bnr-bg-1.jpg",
-        name: "Jobs"
-      };
+  state = {
+    title: "Company Presentation",
+    background:
+      "http://new.uouapps.com/iod-html/html/images/bg/sub-bnr-bg-1.jpg",
+    name: "Presentation"
+  };
 
-    render = () => (
-        <header>
-            <Header></Header>
-            <Pagetop
-          Title={this.state.title}
-          Background={this.state.background}
-          Name={this.state.name}
-        />
-        <PresentationInfo></PresentationInfo>
-            <Footer></Footer>
-            <Bottomfooter></Bottomfooter>
-            </header>
-    )
+  render = () => (
+    <header>
+      <Header></Header>
+      <Pagetop
+        Title={this.state.title}
+        Background={this.state.background}
+        Name={this.state.name}
+      />
+      <Presentationcomponent></Presentationcomponent>
+      <Footer></Footer>
+      <Bottomfooter></Bottomfooter>
+    </header>
+  );
 }
 
 export default Presentation;

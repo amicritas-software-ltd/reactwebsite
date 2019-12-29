4dbc97edad09cfb2c0bdf3fd518776fb34938cb4
Md Sabbir Ahmed <md.sabbir073@gmail.com>
2019-12-29 16:59:03 +0600

    email updated

--- send/index.js
+++ send/index.js
@@ -1,58 +1,58 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var nodemailer = require('nodemailer');
-var cors = require('cors');
-const creds = require('./config');
+var nodemailer = require("nodemailer");
+var cors = require("cors");
+const creds = require("./config");
 
 var transport = {
-    host: 'smtp.gmail.com', // Don’t forget to replace with the SMTP host of your provider
-    port: 465,
-    secure: true,
-    auth: {
+  host: "smtp.gmail.com", // Don’t forget to replace with the SMTP host of your provider
+  port: 465,
+  secure: true,
+  auth: {
     user: creds.USER,
     pass: creds.PASS
   }
-}
+};
 
-var transporter = nodemailer.createTransport(transport)
+var transporter = nodemailer.createTransport(transport);
 
 transporter.verify((error, success) => {
   if (error) {
     console.log(error);
   } else {
-    console.log('Server is ready to take messages');
+    console.log("Server is ready to take messages");
   }
 });
 
-router.post('/send', (req, res, next) => {
-  var name = req.body.name
-  var email = req.body.email
-  var subject = req.body.subject
-  var message = req.body.message
-  var content = `name: ${name} \n email: ${email} \n message: ${message} `
+router.post("/send", (req, res, next) => {
+  var name = req.body.name;
+  var email = req.body.email;
+  var subject = req.body.subject;
+  var message = req.body.message;
+  var content = `name: ${name} \n email: ${email} \n message: ${message} `;
 
   var mail = {
     from: name,
-    to: 'md.sabbir073@gmail.com',  // Change to email address that you want to receive messages on
+    to: "info@amicritas.com", // Change to email address that you want to receive messages on
     subject: subject,
     text: content
-  }
+  };
 
   transporter.sendMail(mail, (err, data) => {
     if (err) {
       res.json({
-        status: 'fail'
-      })
+        status: "fail"
+      });
     } else {
       res.json({
-       status: 'success'
-      })
+        status: "success"
+      });
     }
-  })
-})
+  });
+});
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use('/', router)
-app.listen(3002)
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(3002);
--- src/components/pages/contactUs/ContactForm.js
+++ src/components/pages/contactUs/ContactForm.js
@@ -1,140 +1,151 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 class ContactForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: "",
-            email: "",
-            subject: "",
-            message: "",
-            successalert: "none",
-            dangeralert: "none"
-        }
-      }
-      resetForm(){
-          this.setState({name: ""})
-          this.setState({email: ""})
-          this.setState({subject: ""})
-          this.setState({message: ""})
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+      successalert: "none",
+      dangeralert: "none"
+    };
+  }
+  resetForm() {
+    this.setState({ name: "" });
+    this.setState({ email: "" });
+    this.setState({ subject: "" });
+    this.setState({ message: "" });
+  }
 
-      onNameChange(event) {
-        this.setState({name: event.target.value})
+  onNameChange(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  onEmailChange(event) {
+    this.setState({ email: event.target.value });
+  }
+  onSubjectChange(event) {
+    this.setState({ subject: event.target.value });
+  }
+
+  onMessageChange(event) {
+    this.setState({ message: event.target.value });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+
+    axios({
+      method: "POST",
+      url: "https://amicritas.com/send",
+      data: this.state
+    }).then(response => {
+      if (response.data.status === "success") {
+        this.setState({ successalert: "block" });
+        this.forceUpdate();
+        this.resetForm();
+      } else if (response.data.status === "fail") {
+        this.setState({ dangeralert: "block" });
+        this.forceUpdate();
       }
-    
-      onEmailChange(event) {
-        this.setState({email: event.target.value})
-      }
-      onSubjectChange(event){
-        this.setState({subject: event.target.value})
-      }
-    
-      onMessageChange(event) {
-        this.setState({message: event.target.value})
-      }
-      handleSubmit(e){
-        e.preventDefault();
-    
-        axios({
-          method: "POST", 
-          url:"http://localhost:3002/send", 
-          data:  this.state
-        }).then((response)=>{
-          if (response.data.status === 'success'){
-            this.setState({successalert: "block"})
-            this.forceUpdate();
-            this.resetForm()
-          }else if(response.data.status === 'fail'){
-            this.setState({dangeralert: "block"})
-            this.forceUpdate();
-         }
-        })
-      }
-    render() { 
-        return ( <div>
-          <div className="alert alert-success" style={{display: this.state.successalert}}>
-            <strong>Email sent!</strong> We will contact you soon.
-          </div>
-          <div className="alert alert-danger" style={{display: this.state.dangeralert}}>
-            <strong>Email not sent!</strong> May be there are a problem.
-          </div>
-            <form id="contact_form" onSubmit={this.handleSubmit.bind(this)} className="contact-form" method="post">
-        <ul className="row">
-          <li className="col-sm-12">
-            <label>
-              <input
-                type="text"
-                className="form-control"
-                name="name"
-                id="name"
-                placeholder="Name"
-                value={this.state.name} 
-                onChange={this.onNameChange.bind(this)}
-                required
-              />
-            </label>
-          </li>
-          <li className="col-sm-12">
-            <label>
-              <input
-                type="email"
-                className="form-control"
-                name="email"
-                id="email"
-                placeholder="Email"
-                aria-describedby="emailHelp" 
-                value={this.state.email} 
-                onChange={this.onEmailChange.bind(this)}
-                required
-              />
-            </label>
-          </li>
-          <li className="col-sm-12">
-            <label>
-              <input
-                type="text"
-                className="form-control"
-                name="subject"
-                id="subject"
-                placeholder="Subject"
-                value={this.state.subject}
-                onChange={this.onSubjectChange.bind(this)}
-                required
-              />
-            </label>
-          </li>
-          <li className="col-sm-12">
-            <label>
-              <textarea
-                className="form-control"
-                name="message"
-                id="message"
-                rows="5"
-                placeholder="Message"
-                value={this.state.message} 
-                onChange={this.onMessageChange.bind(this)}
-                required
-              ></textarea>
-            </label>
-          </li>
-          <li className="col-sm-12">
-            <button
-              type="submit"
-              value="submit"
-              className="btn btn-1"
-              id="btn_submit"
-            >
-              Send Message <i className="fa fa-caret-right"></i>
-            </button>
-          </li>
-        </ul>
-        <div>    
+    });
+  }
+  render() {
+    return (
+      <div>
+        <div
+          className="alert alert-success"
+          style={{ display: this.state.successalert }}
+        >
+          <strong>Email sent!</strong> We will contact you soon.
         </div>
-      </form>
+        <div
+          className="alert alert-danger"
+          style={{ display: this.state.dangeralert }}
+        >
+          <strong>Email not sent!</strong> May be there are a problem.
+        </div>
+        <form
+          id="contact_form"
+          onSubmit={this.handleSubmit.bind(this)}
+          className="contact-form"
+          method="post"
+        >
+          <ul className="row">
+            <li className="col-sm-12">
+              <label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="name"
+                  id="name"
+                  placeholder="Name"
+                  value={this.state.name}
+                  onChange={this.onNameChange.bind(this)}
+                  required
+                />
+              </label>
+            </li>
+            <li className="col-sm-12">
+              <label>
+                <input
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  id="email"
+                  placeholder="Email"
+                  aria-describedby="emailHelp"
+                  value={this.state.email}
+                  onChange={this.onEmailChange.bind(this)}
+                  required
+                />
+              </label>
+            </li>
+            <li className="col-sm-12">
+              <label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="subject"
+                  id="subject"
+                  placeholder="Subject"
+                  value={this.state.subject}
+                  onChange={this.onSubjectChange.bind(this)}
+                  required
+                />
+              </label>
+            </li>
+            <li className="col-sm-12">
+              <label>
+                <textarea
+                  className="form-control"
+                  name="message"
+                  id="message"
+                  rows="5"
+                  placeholder="Message"
+                  value={this.state.message}
+                  onChange={this.onMessageChange.bind(this)}
+                  required
+                ></textarea>
+              </label>
+            </li>
+            <li className="col-sm-12">
+              <button
+                type="submit"
+                value="submit"
+                className="btn btn-1"
+                id="btn_submit"
+              >
+                Send Message <i className="fa fa-caret-right"></i>
+              </button>
+            </li>
+          </ul>
+          <div></div>
+        </form>
       </div>
-         );
-    }
+    );
+  }
 }
- 
+
 export default ContactForm;
